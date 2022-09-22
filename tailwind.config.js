1ae9b2c0ee822af528c2ad69a6765a9546ab9d79
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-09-22 03:05:00 +0100

    Update: Add the bills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/*.js"],
   theme: {
+    screens: {
+      sm: "700px",
+      md: "1055px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
         mainBgColor: "#F7F8FA",
@@ -10,6 +16,17 @@ module.exports = {
         lightGray: "#7A7A87",
         smallTextColor: "#888B9D",
         borderColor: "#E7E9FB",
+        greenIndicator: "#008545",
+        redIndicator: "#DD0C0D",
+        blueIndicator: "#495FE9",
+        darkColor: "#050A29",
+        btnAllColor: "#AAB5FA",
+      },
+      keyframes: {
+        bounce: {
+          "0%, 100%": { transform: "translateY(-5px)" },
+          "50%": { transform: "translateY(0px)" },
+        },
       },
     },
     fontFamily: {
